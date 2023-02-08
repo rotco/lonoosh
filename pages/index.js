99aa636874d0c1b2a63b0bbc3b89871025804de3
oren <rotoren@gmail.com>
2023-02-08 22:49:29 +0200

    change localhost to vercel domain

--- pages/index.js
+++ pages/index.js
@@ -79,7 +79,7 @@ function Home({ data }) {
 export default Home;
 
 export async function getServerSideProps() {
-  const res = await axios.get("http://127.0.0.1/api/heros");
+  const res = await axios.get("https://lonoosh.vercel.app/api/heros");
   const data = await res.data.data;
   return { props: { data } };
 }
