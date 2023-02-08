e5e024f9775dcad57d7f5eb7dc6682ba118319e9
oren <rotoren@gmail.com>
2023-02-08 19:21:32 +0200

    removing comments

--- libs/upload_object.js
+++ libs/upload_object.js
@@ -1,10 +1,9 @@
-// Import required AWS SDK clients and commands for Node.js.
 import { PutObjectCommand } from "@aws-sdk/client-s3";
-import { s3Client } from "./s3Client.js"; // Helper function that creates an Amazon S3 service client module.
+import { s3Client } from "./s3Client.js";
 import path from "path";
 import fs from "fs";
 
-const file = "../public/assets/sounds/bad1.mp4"; // Path to and name of object. For example '../myFiles/index.js'.
+const file = "../public/assets/sounds/bad1.mp4";
 const fileStream = fs.createReadStream(file);
 
 export const uploadParams = {
@@ -13,12 +12,11 @@ export const uploadParams = {
   Body: fileStream,
 };
 
-// Upload file to specified bucket.
 export const run = async () => {
   try {
     const data = await s3Client.send(new PutObjectCommand(uploadParams));
     console.log("Success", data);
-    return data; // For unit tests.
+    return data;
   } catch (err) {
     console.log("Error", err);
   }
