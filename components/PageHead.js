6cc3b8b1c703211af4a3a71a5b3c6025dbd7be75
oren <rotoren@gmail.com>
2023-04-29 13:40:35 +0300

    adding link to home

--- components/PageHead.js
+++ components/PageHead.js
@@ -1,8 +1,13 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 export default function PageHead({ width, height }) {
+  const router = useRouter();
+  function handleClick() {
+    router.push("/");
+  }
   return (
-    <div className="alona-banner">
+    <div className="alona-banner" onClick={handleClick}>
       <Image
         src="/assets/images/alona_games_logo_320.jpg"
         width={width ? width : 200}
