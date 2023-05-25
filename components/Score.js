f9a37fb6b856496164eb3649fb3a5f7e840a8cd5
oren <rotoren@gmail.com>
2023-05-26 00:56:05 +0300

    progress styles

--- components/Score.js
+++ components/Score.js
@@ -1,18 +1,38 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import AppContext from "./AppContext";
 import ProgressBar from "react-bootstrap/ProgressBar";
 
 export default function Score() {
   const context = useContext(AppContext);
-  // context.scoreContext * 10
+  const [progressColor, setProgressColor] = useState("danger");
+  useEffect(() => {
+    console.log("useEffect, context.scoreContext", context.scoreContext);
+    console.log("progressColor", progressColor);
+    if (context.scoreContext <= 3) {
+      setProgressColor("danger");
+    } else if (context.scoreContext > 3 && context.scoreContext <= 6) {
+      setProgressColor("warning");
+    } else if (context.scoreContext > 6 && context.scoreContext <= 8) {
+      setProgressColor("info");
+    } else if (context.scoreContext > 8 && context.scoreContext <= 10) {
+      setProgressColor("success");
+    }
+  }, [context.scoreContext]);
+
   return (
     <div className="_score">
-      <ProgressBar
-        now={context.scoreContext * 10}
-        label={`${context.scoreContext * 10}%`}
-        variant="success"
-        animated
-      />
+      <div>ניקוד</div>
+      <div>
+        <ProgressBar
+          now={Math.max(context.scoreContext * 10, 10)}
+          label={`${context.scoreContext * 10}%`}
+          variant={progressColor}
+          animated
+          style={{
+            height: "50px",
+          }}
+        />
+      </div>
     </div>
   );
 }
